8431b5c81effa121d84560cfd80ced351ddc9d78
ZakiMohammed <shaikhzaki67@gmail.com>
2018-10-16 18:19:27 +0530

    jwt token implementation

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 // bodyParser middleware
 app.use(bodyParser.json());
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
 app.use('/users', users);
 
 // index route
@@ -38,3 +43,5 @@ app.get('/', (request, response) =>{
 app.listen(port, () => {
     console.log('Server started on port ' + port)
 });
+
+// Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjViYzVkNzA0ZWI4NTk5M2IyYzdkODYyNCIsIm5hbWUiOiJKb2huIE1hcnNoYWwiLCJlbWFpbCI6ImpvaG5AZ21haWwuY29tIiwidXNlcm5hbWUiOiJqb2huIiwicGFzc3dvcmQiOiIkMmEkMTAkSC4xbXhzZ3ROblo0MWtzSThQMVByLmkyTTN3OWhuOEh3MGtSd1ZqZGw3Y1piN1E3L3pSNjYiLCJfX3YiOjB9LCJpYXQiOjE1Mzk2OTMyOTUsImV4cCI6MTU0MDI5ODA5NX0.K8guAsxL5QyJuGwwPxC-K5Bjg78mSj2FwkdP9NiQkIc
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,59 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+const User = require('../models/user');
 
-router.get('/register', (request, response, next) => {
-    response.send('Register');
+router.post('/register', (request, response, next) => {
+    let newUser = new User({
+        name: request.body.name,
+        email: request.body.email,
+        username: request.body.username,
+        password: request.body.password
+    });
+    User.addUser(newUser, (error, user) => {
+        if (error) {
+            response.json({ success: false, msg: 'Failed to register user' });
+        } else {
+            response.json({ success: true, msg: 'User registered' });
+        }
+    });
 });
 router.post('/authenticate', (request, response, next) => {
-    response.send('Authenticate');
-});
-router.get('/profile', (request, response, next) => {
-    response.send('Profile');
-});
+    const username = request.body.username;
+    const password = request.body.password;
 
+    User.getUserByUsername(username, (error, user) => {
+        if (error) throw error;
+        if (!user)
+            return response.json({ success: false, msg: 'User not found' });
+
+        User.comparePassword(password, user.password, (error, isMatch) => {
+            if (error) throw error;
+            if (isMatch) {
+
+                const token = jwt.sign({ data: user }, config.secret, {
+                    expiresIn: 604800
+                });
+
+                response.json({
+                    success: true,
+                    token: `Bearer ${token}`,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                    }
+                });
+            } else {
+                return response.json({ success: false, msg: 'Wrong password' });
+            }
+        });
+    });
+});
+router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    res.json({ user: req.user });
+});
 module.exports = router;
